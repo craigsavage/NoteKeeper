77f7debf546c6b7b59731d92e60ca7f0c17c88ef
craigsavage <craig.savage@ontariotechu.net>
2020-05-08 02:16:07 -0400

    Added hooks for note creation state

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 function CreateArea() {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleNote(event) {
+    const {name, value} = event.target;
+
+    setNote(prev => {
+      return {
+        ...prev,
+        [name]: value
+      }      
+    })
+    
+  }
+  console.log(note)
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+      <form >
+        <input name="title" value={note.title} onChange={handleNote} placeholder="Title" />
+        <textarea name="content" value={note.content} onChange={handleNote}  placeholder="Take a note..." rows="3" />
         <button>Add</button>
       </form>
     </div>
