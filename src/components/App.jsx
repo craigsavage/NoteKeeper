0a0e5bc7218ab7425bb02ab88535e67bf44c9f93
craigsavage <craig.savage@ontariotechu.net>
2020-05-08 19:47:02 -0400

    Added functionality for showing/hiding input form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([])
+
+  // Adds a note to the notes array
+  function addNote(note) {
+    setNotes(prev => [...prev, note]);
+  }
+
+  // Removes a note from the notes array
+  function deleteNote(id) {
+    setNotes(prev => {
+      return prev.filter((note, index) => index !== id)
+    });
+  }
+  console.log(notes)
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea addNote={addNote} />
+      {notes.map((note, index) => (
+        <Note key={index} id={index} title={note.title} content={note.content} deleteNote={deleteNote} />
+      ))}
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,29 +1,51 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import Zoom from '@material-ui/core/Zoom';
 
-function CreateArea() {
+
+function CreateArea(props) {
+  const [isTyping, setIsTyping] = useState(false);
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
 
   function handleNote(event) {
-    const {name, value} = event.target;
+    const { name, value } = event.target;
 
     setNote(prev => {
       return {
         ...prev,
         [name]: value
-      }      
+      }
     })
-    
+  }
+
+  function submitNote(event) {
+    event.preventDefault();
+    props.addNote(note);
+    setNote({
+      title: "",
+      content: ""
+    });
   }
   console.log(note)
+
+  function typingHandler() {
+    setIsTyping(true)
+  }
+
   return (
     <div>
-      <form >
-        <input name="title" value={note.title} onChange={handleNote} placeholder="Title" />
-        <textarea name="content" value={note.content} onChange={handleNote}  placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form className="create-note">
+        {isTyping && <input name="title" value={note.title} onChange={handleNote} placeholder="Title" />}        
+        <textarea name="content" value={note.content} onChange={handleNote} onClick={typingHandler} placeholder="Take a note..." rows={isTyping ? 3 : 1} />
+        <Zoom in={isTyping} timeout={600}>
+          <Fab onClick={submitNote} aria-label="add">
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
